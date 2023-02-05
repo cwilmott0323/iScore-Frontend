f9db9e6d5abeb5e80cb6dd86d9b516c4a7ed2628
Chris Wilmott <admin@sosurprises.com>
2023-02-05 18:09:31 -0500

    update api url

--- src/Login.js
+++ src/Login.js
@@ -14,7 +14,7 @@ async function loginUser(credentials) {
 
     try {
         return {
-            token: await axios.post('http://localhost:5001/accounts/login', JSON.stringify(credentials)),
+            token: await axios.post('https://sj9klp6ugk.execute-api.us-east-2.amazonaws.com/accounts/login', JSON.stringify(credentials)),
             error: null
         }
 
